b5214b75bd337308049bf9fdb48ca4cb347f0344
Jordan White <jlwhite1991@gmail.com>
2019-04-11 11:43:24 -0400

    Added the ability to submit a service request - completed the Service Request card

---
+++ Capstone/Controllers/TenantController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.DAL;
+using Capstone.DAL.Interfaces;
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class TenantController : Controller
+    {
+        private IServiceRequestDAL serviceRequestDAL;
+
+        public TenantController(IServiceRequestDAL serviceRequestDAL)
+        {
+            this.serviceRequestDAL = serviceRequestDAL;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ServiceRequest serviceRequest = new ServiceRequest();
+            return View(serviceRequest);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Index(ServiceRequest serviceRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(serviceRequest);
+            }
+            else
+            {
+                serviceRequestDAL.AddServiceRequest(serviceRequest);
+
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
