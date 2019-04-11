af6dddc4d69d1d58402096506eb1f6ab63cfbaa5
Chris Graham <cgraham.osu@gmail.com>
2019-04-11 15:40:42 -0400

    Deleted old linq queries for advanced search. Though we can get then to work by adding .Any() to the end of the inner lambda.

--- Capstone/Models/SearchModel.cs
+++ Capstone/Models/SearchModel.cs
@@ -22,63 +22,7 @@ namespace Capstone.Models
 
         public void HandleAdvancedSearch()
         {
-            List<Property> result = new List<Property>();
-
-            foreach (Property property in AvailableProperties)
-            {
-                foreach (Unit unit in property.UnitsAtThisProperty)
-                {
-                    if (NumberofBathsMin > 0)
-                    {
-                       
-
-                    }
-
-                }
-
-
-            }
-
-            return result;
-
-            ////HandleBedNumber
-            //if (NumberofBathsMin > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.NumberOfBaths >= NumberofBathsMin) != null).ToList();
-            //}
-            //if (NumberofBathsMax > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.NumberOfBaths <= NumberofBathsMax) != null).ToList();
-            //}
-            //if (NumberofBedsMin > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.NumberOfBeds >= NumberofBedsMin) != null).ToList();
-            //}
-            //if (NumberofBedsMax > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.NumberOfBeds <= NumberofBedsMax) != null).ToList();
-            //}
-            //if (MonthlyRentMin > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.MonthlyRent >= MonthlyRentMin) != null).ToList();
-            //}
-            //if (MonthlyRentMax > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.MonthlyRent <= MonthlyRentMax) != null).ToList();
-            //}
-            //if (SquareFeetMin > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.SquareFeet >= SquareFeetMin) != null).ToList();
-
-            //}
-            //if (SquareFeetMax > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.SquareFeet <= SquareFeetMax) != null).ToList();
-            //}
-            //if (ZipCode > 0)
-            //{
-            //    AvailableProperties = AvailableProperties.Where(property => property.UnitsAtThisProperty.Where(unit => unit.ZipCode == ZipCode) != null).ToList();
-            //}
+            
         }
 
 
