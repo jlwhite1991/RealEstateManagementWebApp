7d10f92a01b8ca927d5c77ea3bc3087f4b287bb5
Chris Graham <cgraham.osu@gmail.com>
2019-04-16 11:56:00 -0400

    Created UserDAL for handling the user table, and in the process of implementing users and AUthorization to the Capstone. Providers folder contains Authorization. Added Model for user in User.cs

---
+++ Capstone/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Capstone.Models;
+using Capstone.Models.Account;
+using Capstone.Providers.Auth;
+
+namespace Capstone.Controllers
+{
+
+    public class AccountController : Controller
+    {
+    private readonly IAuthProvider authProvider;
+    public AccountController(IAuthProvider authProvider)
+    {
+        this.authProvider = authProvider;
+    }
+
+
+
+        [AuthorizationFilter("manager", "owner", "tenant", "user")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            User user = authProvider.GetCurrentUser();
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                bool validLogin = authProvider.SignIn(loginViewModel.Email, loginViewModel.Password);
+                if(validLogin)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult LogOff()
+        {
+            authProvider.LogOff();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register (RegisterViewModel registerViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+               
+
+            }
+            return View();
+        }
+        
+
+    }
+}
